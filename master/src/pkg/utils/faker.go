eae3e7724c8f2c9b58ad02381c3449584a770649
Maximilian <66524685+maxbirnbacher@users.noreply.github.com>
2024-05-08 20:14:05 +0200

    removed faker lib
    i can also do it without faker

--- master/src/pkg/utils/faker.go
+++ master/src/pkg/utils/faker.go
@@ -4,17 +4,18 @@ import (
     "fmt"
     "net/http"
     "strings"
-    "github.com/go-faker/faker/v4"
 )
 
 func GeneratePathAndURL(r *http.Request) (string, string) {
-    // Generate a fake URI path
-    path := faker.Word()
-    // Use only the part after the last slash
-    path = path[strings.LastIndex(path, "/")+1:]
-    fmt.Println("Generated path: ", path)
-    // Get the URL from the query parameters
-    url := r.Form.Get("url")
+    // TODO: NEEDS REWRITE
+    
+    // // Generate a fake URI path
+    // path := faker.Word()
+    // // Use only the part after the last slash
+    // path = path[strings.LastIndex(path, "/")+1:]
+    // fmt.Println("Generated path: ", path)
+    // // Get the URL from the query parameters
+    // url := r.Form.Get("url")
 
-    return path, url
+    // return path, url
 }
--- master/src/pkg/utils/go.mod
+++ master/src/pkg/utils/go.mod
@@ -2,9 +2,4 @@ module utils
 
 go 1.22
 
-require (
-	github.com/go-faker/faker/v4 v4.2.1
-	github.com/google/uuid v1.6.0
-)
-
-require golang.org/x/text v0.3.7 // indirect
+require github.com/google/uuid v1.6.0
